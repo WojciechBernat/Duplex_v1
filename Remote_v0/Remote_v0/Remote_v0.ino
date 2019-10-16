178ecfeb4c5b1bece2f6dc93e3850dfa4ea1935c
Khevenin <wojciechbernat95@gmail.com>
2019-10-16 22:29:02 +0200

    Add list
    Added TX and RX buffers
    Added TX and RX adresses array
    UART init
    TX and RX LEDs pin init

--- Remote_v0/Remote_v0/Remote_v0.ino
+++ Remote_v0/Remote_v0/Remote_v0.ino
@@ -13,18 +13,38 @@
 #include <RF24.h>
 
 /* Directives and Macros */
+#define UART_SPEED_48 4800
+#define UART_SPEED_96 9600
+#define UART_SPEED_288 28800
+#define UART_SPEED_115 115200
 
+#define TX_PIN_LED 6
+#define RX_PIN_LED 5
 
 /* Variables */
 
 /* Arrays */
+uint8_t TxBuffer[32];
+uint8_t RxBuffer[32];
+
+uint8_t TxAddresses[5];
+uint8_t RxAddresses[5];
 
 /* Prototypes */
 
 /* Init */
-RF24 radio(7, 8 );    //Create an RF24 object - set CE - pin 7, CN - pin 8
+RF24 radio(7, 8);    //Create an RF24 object - set CE - pin 7, CN - pin 8
 
 void setup() {
+  /* Serial port init */
+  Serial.begin(UART_SPEED_96, SERIAL_8E1);      //UART 8 bits with EVEN mode - że bit parzystości
+  Serial.println("\nRemote application start\nUART init OK\n");
+
+  /* Pin init */
+  pinMode(TX_PIN_LED, OUTPUT);
+  pinMode(RX_PIN_LED, OUTPUT);
+  Serial.println("\nTX, RX LED pins OK\n");
+  /* add blink function in this place */
   
 
 }
@@ -33,3 +53,6 @@ void loop() {
   
 
 }
+
+
+/* Functions */
