cbc96efb2f916bb4df4361906abc51f878753c09
Khevenin <wojciechbernat95@gmail.com>
2019-10-16 21:26:38 +0200

    Second init commit
    Added init comment 
    Added libraries section

--- Remote_v0/Remote_v0/Remote_v0.ino
+++ Remote_v0/Remote_v0/Remote_v0.ino
@@ -1,9 +1,26 @@
+/* 
+ *  Program z uzyciem nRF24L01+ pod uC Atmega 328 (Arduino Uno Rev3)
+ *  Przesylanie danych z urzadzenia nadrzednego do odbiornika.
+ *  Biblioteka: TMRh20/RF24
+ *  Urzadzdenie: Master
+ *  Autor: Khevenin
+ */
+
+/* Libraries */
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+/* Directives and Macros */
+
+
 void setup() {
-  // put your setup code here, to run once:
+  
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
 }
