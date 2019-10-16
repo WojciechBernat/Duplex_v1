975b1ebbe0f245db570284d0c24bc8c9b4dd700d
Khevenin <wojciechbernat95@gmail.com>
2019-10-16 21:39:01 +0200

    Added directives
    Added directives of SPI speed
    Create RF24 object

--- Remote_v0/Remote_v0/Remote_v0.ino
+++ Remote_v0/Remote_v0/Remote_v0.ino
@@ -13,7 +13,18 @@
 #include <RF24.h>
 
 /* Directives and Macros */
+#define SPI_SPEED_LOW       2   //2 MHz for RF24 instance's constructor
+#define SPI_SPEED_MEDIUM    4   //4 MHz
+#define SPI_SPEED_HIGH      8   //8 MHz
 
+/* Variables */
+
+/* Arrays */
+
+/* Prototypes */
+
+/* Init */
+RF24 radio(7, 8, SPI_SPEED_LOW );    //Create an RF24 object - set CE - pin 7, CN - pin 8, SPI bus speed 
 
 void setup() {
   
