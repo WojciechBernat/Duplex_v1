b8176718c70d40304a1d9de554e21b59f987622f
Khevenin <wojciechbernat95@gmail.com>
2019-10-17 23:13:08 +0200

    Create one pins init function
    One function to init OUTPUT pins void pinsInit( uint8_t LedPin_1, uint8_t LedPin_2, String Name_1, String Name_2 ) with previously created functions

--- Receiver_v0/Receiver_v0/Receiver_v0.ino
+++ Receiver_v0/Receiver_v0/Receiver_v0.ino
@@ -25,6 +25,8 @@
 String TxLedName = "TX LED";
 String RxLedName = "RX LED";
 
+uint16_t defaultBlinkTime = 200;
+
 /* Arrays */
 uint8_t TxBuffer[32];
 uint8_t RxBuffer[32];
@@ -33,10 +35,12 @@ uint8_t TxAddresses[5] = {0x0B, 0x0B, 0x0B, 0x0B, 0x02};    //Zmiana adresów wz
 uint8_t RxAddresses[5] = {0x0A, 0x0A, 0x0A, 0x0A, 0x01};
 
 /* Prototypes */
-void Blink(uint8_t ledPin = 0, uint16_t blinkTime = 200 );  //Funkcja z wartosciami domyslnymi
-doubleBlink(uint8_t ledPin_1, uint8_t ledPin_2, uint16_t blinkTime)
-void bufferReset(uint8_t *buf, uint8_t bufSize);
+boolean Blink(uint8_t ledPin = 0, uint16_t blinkTime = defaultBlinkTime);                       //Funkcja z wartosciami domyslnymi
+boolean doubleBlink(uint8_t ledPin_1, uint8_t ledPin_2, uint16_t blinkTime = defaultBlinkTime);
 void pinsInitPrint(String pinNum, String Name);
+void pinsInit( uint8_t LedPin_1, uint8_t LedPin_2, String Name_1, String Name_2 );
+void bufferReset(uint8_t *buf, uint8_t bufSize);
+
 
 /* Init */
 
@@ -48,14 +52,7 @@ void setup() {
   Serial.println("\nRemote application start\nUART init OK\n");
 
   /* Pin init */
-  pinMode(TX_PIN_LED, OUTPUT);
-  pinMode(RX_PIN_LED, OUTPUT);
-  pinsInitPrint(String(TX_PIN_LED), TxLedName);
-  pinsInitPrint(String(RX_PIN_LED), RxLedName);
-  doubleBlink
-// Blink(TX_PIN_LED);
-// Blink(RX_PIN_LED);
-
+ pinsInit( TX_PIN_LED, RX_PIN_LED, TxLedName, RxLedName );
 
 
 
@@ -68,6 +65,27 @@ void loop() {
 
 
 /* Functions */
+
+boolean Blink(uint8_t ledPin, uint16_t blinkTime) {
+  if ((ledPin < 0) || (ledPin > 13)) {
+    return false;
+  }
+  if ( blinkTime > 10000) {   //jezeli wieksze do 10 sek
+    blinkTime = 2000;
+  }
+  if ( digitalRead(ledPin) == HIGH) {
+    digitalWrite(ledPin, LOW);
+    delay(blinkTime);
+    digitalWrite(ledPin, HIGH);
+    return true;
+  }
+  else {
+    digitalWrite(ledPin, HIGH);
+    delay(blinkTime);
+    digitalWrite(ledPin, LOW);
+    return true;
+  }
+}
 
 boolean doubleBlink(uint8_t ledPin_1, uint8_t ledPin_2, uint16_t blinkTime) {
   if ( ((ledPin_1 < 0) || (ledPin_1 > 13)) || ((ledPin_2 < 0) || (ledPin_2 > 13)) ) {
@@ -105,30 +123,21 @@ boolean doubleBlink(uint8_t ledPin_1, uint8_t ledPin_2, uint16_t blinkTime) {
   }
 }
 
-void Blink(uint8_t ledPin, uint16_t blinkTime) {
-  if ((ledPin < 0) || (ledPin > 13)) {
-    ledPin = 0;
-  }
-  if ( blinkTime > 10000) {   //jezeli wieksze do 10 sek
-    blinkTime = 2000;
-  }
-  if ( digitalRead(ledPin) == HIGH) {
-    digitalWrite(ledPin, LOW);
-    delay(blinkTime);
-    digitalWrite(ledPin, HIGH);
-  }
-  else {
-    digitalWrite(ledPin, HIGH);
-    delay(blinkTime);
-    digitalWrite(ledPin, LOW);
-  }
+void pinsInitPrint(String pinNum, String Name) {
+  Serial.println("Correct initialization of pin " +  pinNum  + " : " + Name);
 }
+
+void pinsInit( uint8_t LedPin_1, uint8_t LedPin_2, String Name_1, String Name_2 ) {
+  pinMode(LedPin_1, OUTPUT);
+  pinMode(LedPin_2, OUTPUT);
+  pinsInitPrint(String(LedPin_1), Name_1);   //toDebug
+  pinsInitPrint(String(LedPin_2), Name_2);   //toDebug
+  doubleBlink(LedPin_1, LedPin_2);           //toDebug
+}
+
 
 void bufferReset(uint8_t *buf, uint8_t bufSize) {            //Funkcja resetowanai bufora
   for (uint8_t i = 0; i < bufSize; i++) {
     buf[i] = 0;
   }
-}
-void pinsInitPrint(String pinNum, String Name) {
-  Serial.println("Correct initialization of pin " +  pinNum  + " : " + Name);
 }
