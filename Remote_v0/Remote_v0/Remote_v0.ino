402da42f789871d87b454c40a579c2e86d319b6e
Khevenin <wojciechbernat95@gmail.com>
2019-10-19 05:30:47 +0200

    Added radio setting
    Init radio 
    Set TX and RX addresses
    Set channel, power, data rate
    Added functions to print settings

--- Remote_v0/Remote_v0/Remote_v0.ino
+++ Remote_v0/Remote_v0/Remote_v0.ino
@@ -13,6 +13,9 @@
 #include <RF24.h>
 
 /* Directives and Macros */
+#define PIPE_ADDRESS_SIZE  5    //Only 5 byte!
+#define BUFFER_SIZE        32   //
+
 #define UART_SPEED_48 4800
 #define UART_SPEED_96 9600
 #define UART_SPEED_288 28800
@@ -29,12 +32,17 @@ String RxBufName = "RxBuffer";
 
 uint16_t defaultBlinkTime = 0xFF;  // value of blink time - 255ms ;
 
-/* Arrays */
-uint8_t TxBuffer[32];
-uint8_t RxBuffer[32];
+uint8_t RTxChannel = 64;              // 0 - 124
+uint8_t TxPowerLevel = RF24_PA_MIN;   //RF24_PA_MIN - -18dBm, RF24_PA_LOW - -12dBm, RF24_PA_HIGH - -6dBm, RF24_PA_MAX - 0dBm
+//RF24_250KBPS for 250kbs, RF24_1MBPS for 1Mbps, or RF24_2MBPS for 2Mbps
 
-uint8_t TxAddresses[5] = {0x0A, 0x0A, 0x0A, 0x0A, 0x01};
-uint8_t RxAddresses[5] = {0x0B, 0x0B, 0x0B, 0x0B, 0x02};
+
+/* Arrays */
+uint8_t TxBuffer[BUFFER_SIZE];
+uint8_t RxBuffer[BUFFER_SIZE];
+
+uint8_t TxAddresses[PIPE_ADDRESS_SIZE] = {0x0A, 0x0A, 0x0A, 0x0A, 0x01};
+uint8_t RxAddresses[PIPE_ADDRESS_SIZE] = {0x0B, 0x0B, 0x0B, 0x0B, 0x02};
 
 /* Prototypes */
 boolean Blink(uint8_t ledPin = 0, uint16_t blinkTime = defaultBlinkTime);                       //Funkcja z wartosciami domyslnymi
@@ -45,6 +53,11 @@ void pinsInit( uint8_t LedPin_1, uint8_t LedPin_2, String Name_1, String Name_2 
 void bufferReset(uint8_t *buf, uint8_t bufSize);
 void bufferResetPrint( String bufferName);
 void cleanBuffers(uint8_t *buf_1, uint8_t bufSize_1, uint8_t *buf_2, uint8_t bufSize_2, String bufferName_1, String bufferName_2 );
+
+void channelPrint(uint8_t channel);
+void powerLevelPrint( rf24_pa_dbm_e power );
+void dataratePrint( rf24_datarate_e rate );
+void radioInit(uint8_t *TxADDR, uint8_t RxADDR, uint8_t channel,  rf24_pa_dbm_e power, rf24_datarate_e rate  );
 
 /* Init */
 RF24 radio(7, 8);    //Create an RF24 object - set CE - pin 7, CN - pin 8
@@ -58,6 +71,22 @@ void setup() {
   pinsInit( TX_PIN_LED, RX_PIN_LED, TxLedName, RxLedName );
   /* Buffer reset - clean cells of arrays */
   cleanBuffers(TxBuffer, sizeof(TxBuffer), RxBuffer, sizeof(RxBuffer), TxBufName, RxBufName);
+
+  /* Radio init */
+  radio.begin();
+  radio.openWritingPipe( TxAddresses );         //TX pipe address
+  Serial.println("\nSet TxAddress Pipeline ");
+  radio.openReadingPipe( 1, RxAddresses );      //RX pipe address
+  Serial.println("\nSet RxAddress Pipeline ");
+  
+  radio.setChannel(RTxChannel);                 //Set TX/RX channel
+  channelPrint(RTxChannel);
+
+  radio.setPALevel(RF24_PA_MIN) ;              //Set TX output power
+  powerLevelPrint(RF24_PA_MIN);
+
+  radio.setDataRate(RF24_250KBPS);            //Set TX speed
+  dataratePrint(RF24_250KBPS);
 
 
 }
@@ -139,6 +168,7 @@ void pinsInit( uint8_t LedPin_1, uint8_t LedPin_2, String Name_1, String Name_2 
   doubleBlink(LedPin_1, LedPin_2);           //toDebug
 }
 
+
 void bufferReset(uint8_t *buf, uint8_t bufSize) {            //Funkcja resetowanai bufora
   for (uint8_t i = 0; i < bufSize; i++) {
     buf[i] = 0;
@@ -154,4 +184,44 @@ void cleanBuffers(uint8_t *buf_1, uint8_t bufSize_1, uint8_t *buf_2, uint8_t buf
   bufferResetPrint( bufferName_1);
   bufferReset(buf_2, bufSize_2);
   bufferResetPrint( bufferName_2);
+}
+
+
+void channelPrint(uint8_t channel) {
+  Serial.println("\nSet RX & TX channel: " + (String(channel)));
+}
+
+void powerLevelPrint( rf24_pa_dbm_e power ) {
+
+  if (power == 0) {
+    Serial.println("\nSet TX power: -18dBm");
+  }
+  else if (power == 1) {
+    Serial.println("\nSet TX power: -12dBm");
+  }
+  else if ( power == 2) {
+    Serial.println("\nSet TX power: -6dBm");
+  }
+  else if ( power == 3) {
+    Serial.println("\nSet TX power: 0dBm");
+  }
+}
+
+void dataratePrint( rf24_datarate_e rate ) {
+  if (rate == 0) {
+    Serial.println("\nSet data rate: 1Mbps");
+  }
+  else if (rate== 1) {
+    Serial.println("\nSet data rate: 2Mbps");
+  }
+  else if (rate == 2) {
+    Serial.println("\nSet data rate: 250kbps");
+  }
+}
+
+
+
+void radioInit(uint8_t *TxADDR, uint8_t RxADDR, uint8_t channel,  rf24_pa_dbm_e power, rf24_datarate_e rate  ) {
+
+
 }
